9c5a0e165afcf076d422d4fff14a2bbd0f207072
Saurav Bhuju <you@example.com>
2018-03-29 20:53:34 -0600

    incomplete

--- fuel/app/views/ct310/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo Form::button('name', 'value', array('style' => 'border: 2px;'));
-
-?>
--- fuel/app/views/posts/index.php
+++
@@ -1,1 +0,0 @@
-<p> This website is part of a CSU CT310 Project. Our goal was to create a tour attraction website about Kansas and this is our product. We hope that you enjoy our product! </p>
