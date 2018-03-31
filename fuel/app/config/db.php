1629c86ce90657dd60103e47a92012b66ceaec87
Saurav Bhuju <you@example.com>
2018-03-30 18:07:06 -0600

    fuelphp sql (integration)

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -6,5 +6,22 @@
  */
 
 return array(
-
+    'development' => array(
+		'type'           => 'mysqli',
+		'connection'     => array(
+			'hostname'       => 'localhost',
+			'port'           => '3306',
+			'database'       => 'ct310',
+			'username'       => 'ct310',
+			'password'       => 'ct310class',
+			'persistent'     => false,
+			'compress'       => false,
+		),
+		'identifier'     => '`',
+		'table_prefix'   => '',
+		'charset'        => 'utf8',
+		'enable_cache'   => true,
+		'profiling'      => false,
+		'readonly'       => false,
+	),
 );
