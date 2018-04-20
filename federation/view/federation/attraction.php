c45c22991d934dde1b9d933c42361d40af477247
sbhuju <31581665+sbhuju@users.noreply.github.com>
2018-04-20 12:08:06 -0600

    Delete attraction.php

--- federation/view/federation/attraction.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$arr = array(array('id' => "10",'name' => "Eiteljorg Museum of American Indians and Western Art", 'state' => "IN"));
-$run = Format::forge($arr)->to_json();
-
-return $run;
-?>
