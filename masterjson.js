f0d1c0a413035787e9ed068410292e88e8427df7
mirrorad <36482289+ct310mirro@users.noreply.github.com>
2018-04-21 17:20:04 -0600

    Updated masterjson with proper status and styling

--- masterjson.js
+++ masterjson.js
@@ -1,60 +1,79 @@
-+$key=0;
-+$status_data = "";
-+$.ajax({
-+dataType: "json",
-+url: "http://www.cs.colostate.edu/~ct310/yr2018sp/master.json",
-+data: {},
-+success : function(data) {
-+
-+$student_data = "";
-+$.each(data, function(key, value) {
-+	
-+	$.ajax({
-+	$dataType: "json",
-+	$contentType: "application/json",
-+	url: "http://www.cs.colostate.edu/~"+value.eid+"/ct310/index.php/federation/status",
-+	$data: {},
-+	error: function(XMLHttpRequest, textStatus, errorThrown) { 
-+		$key++;
-+      	$("#status"+$key).text("Error");
-+	},
-+	success : function(data, textStatus, xhr) {
-+	$key++	;
-+	try{
-+		$status_data = "";
-+		if (xhr.status == 200) {
-+			if(typeof data =="object")
-+			{
-+			 $("#status"+$key).text(data.status);
-+			}
-+			else
-+			{
-+			 // Response is HTML
-+			 $data = JSON.parse(data);
-+			 $("#status"+$key).text($data.status);
-+			}
-+		} 
-+		else {
-+			$("#status"+$key).text("Error"+$key);
-+		}
-+	}
-+	catch(e)
-+	{
-+		$("#status"+$key).text("Error");
-+	}
-+
-+	$('#status_data').append(status_data);
-+}
-+});
-+	
-+	student_data += '<tr>';
-+	student_data += '<td>' + value.team+'</td>';
-+	student_data += '<td>' + value.nameShort+'</td>';
-+	student_data += '<td>' + value.nameLong+'</td>';
-+	student_data += '<td>' + value.eid +'</td>';
-+	student_data += '<td id = status'+key+'></td>'; 
-+});
-+$('#student_data').append(student_data);
-+}
-+});
-+ 
+$key=0;
+
+
+$.ajax({	
+dataType: "json",
+url: "http://www.cs.colostate.edu/~ct310/yr2018sp/master.json",
+data: {}
+})
+.then(function(data) {
+
+$.each(data, function(key, value) {
+	$status_data = "";
+	$.ajax({
+	$dataType: "json",
+	$contentType: "application/json",
+	url: "http://www.cs.colostate.edu/~"+value.eid+"/ct310/index.php/federation/status",
+})
+.then(function(data, textStatus, xhr) {
+	//$key++;
+	try{
+	 
+		if (xhr.status == 200) {
+			if(typeof data =="object")
+			{
+			if (data.status == "open") {
+			$("#status"+key).text(data.status).css({"background-color": "green", "border-style": "groove", "border-color": "black", "border-width": "1px"});
+		 }
+			else {
+		 $("#status"+key).text(data.status).css({"background-color": "red", "border-style": "groove", "border-color": "black", "border-width": "1px"});
+		 }
+			// $("#status"+key).text(data.status);
+	
+			}
+			else
+			{
+			 // Response is HTML
+			 	 
+			 $data = JSON.parse(data);
+			 	if ($data.status == "open") {
+			 $("#status"+key).text($data.status).css({"background-color": "green", "border-style": "groove", "border-color": "black", "border-width": "1px"});
+		 }
+			else {
+ $("#status"+key).text($data.status).css({"background-color": "red", "border-style": "groove", "border-color": "black", "border-width": "1px"});
+		 }
+			 //$("#status"+key).text($data.status);
+		
+			}
+			
+		}
+		else {
+	
+				$("#status"+key).text("Error").css({"background-color": "yellow", "border-style": "groove", "border-color": "black", "border-width": "1px"});
+				
+	}
+}
+	catch(e)
+	{
+		$("#status"+key).text("Error").css({"background-color": "yellow", "border-style": "groove", "border-color": "black", "border-width": "1px"});
+		
+	}
+
+	
+}).fail(function(xhr){
+	$("#status"+key).text("Error").css({"background-color": "yellow", "border-style": "groove", "border-color": "black", "border-width": "1px"});
+});
+	
+	student_data += '<tr>';
+	student_data += '<td>' + value.team+'</td>';
+	student_data += '<td>' + value.nameShort+'</td>';
+	student_data += '<td>' + value.nameLong+'</td>';
+	student_data += '<td>' + value.eid +'</td>';
+	student_data += '<td id = status'+key+'></td>'; 
+});
+$('#student_data').append(student_data);
+})
+.fail(function(xhr) {
+	console.log('error',xhr);
+});
+
